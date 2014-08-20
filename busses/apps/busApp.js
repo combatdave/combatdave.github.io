f07a8f8f81b2f59be9d18a665b0041a2e412fc7e
combatdave <davidgundry@outlook.com>
2014-08-20 17:06:29 +0000

    More fancyness

--- busses/apps/busApp.js
+++ busses/apps/busApp.js
@@ -10,6 +10,11 @@ function BusInfo(route, busInfo){
 	this.y = busInfo.y;
 	this.from = busInfo.from;
 	this.to = busInfo.to;
+	this.routeText = "Unknown";
+	if (this.from.length > 1 && this.to.length > 1)
+	{
+		this.routeText = "From " + this.from + " to " + this.to;
+	}
 };
 
 
@@ -26,40 +31,67 @@ app.controller("myController", [
 		$scope.surname = "Best";
 		$scope.busses = [1, 2, 3, 14];
 		$scope.jsonthing = "nothing yet";
+		$scope.routes = []
 		$scope.bussesByRoute = {};
 		var markers = [];
 
 
+		$scope.ViewBus = function(bus) {
+			map.panTo(new google.maps.LatLng(bus.x, bus.y));
+			map.setZoom(16);
+		};
+
+
+		$scope.BusClicked = function(bus) {
+			$scope.ViewBus(bus);
+			$("#collapse" + bus.route).collapse(true);
+		}
+
+
 		$scope.UpdateBusses = function() { 
-			BusListing.GetBusses().then(function(busses) {
+			BusListing.GetBusses().then(function(bussesByRoute) {
 				$scope.jsonthing = "";
-				$scope.bussesByRoute = busses;
-				$scope.CreateMarkers();
+				$scope.routes = Object.keys(bussesByRoute);
+				var oldBussesByRoute = $scope.bussesByRoute;
+				$scope.bussesByRoute = bussesByRoute;
+				$scope.CreateMarkers(bussesByRoute, oldBussesByRoute);
 			});
 		};
 
 
-		$scope.CreateMarkers = function() {
+		function CreateMarker(bus) {
+			var marker = new google.maps.Marker({
+				position: new google.maps.LatLng(bus.x, bus.y),
+				title: "Route " + bus.route.toString() + ", from " + bus.from + " to " + bus.to + ".",
+				bus: bus
+			});
+
+			google.maps.event.addListener(marker, 'click', function() {
+				$scope.BusClicked(marker.bus);
+			});
+
+			marker.setMap(map);
+
+			return marker;
+		}
+
+
+		$scope.CreateMarkers = function(bussesByRoute, oldBussesByRoute) {
 			for (var i = 0; i < markers.length; i++) {
 				markers[i].setMap(null);
 			}
 			markers = [];
 
 			console.log("Updatemap")
-			var routeIDs = Object.keys($scope.bussesByRoute);
+			var routeIDs = Object.keys(bussesByRoute);
 			for (var i = 0; i < routeIDs.length; i++)
 			{
 				var routeID = routeIDs[i];
-				var bussesOnRoute = $scope.bussesByRoute[routeID];
+				var bussesOnRoute = bussesByRoute[routeID];
 				for (var j = 0; j < bussesOnRoute.length; j++)
 				{
 					var bus = bussesOnRoute[j];
-					var marker = new google.maps.Marker({
-						position: new google.maps.LatLng(bus.x, bus.y),
-						title: "Route " + bus.route.toString() + ", from " + bus.from + " to " + bus.to + ".",
-					});
-					marker.setMap(map);
-					markers.push(marker);
+					markers.push(CreateMarker(bus));
 				}
 			}
 		};
